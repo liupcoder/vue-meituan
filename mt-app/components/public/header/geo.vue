20f68c2178babeb147e07f3b8760d5aeca6f44f3
daochun <>
2020-01-13 18:58:11 +0800

    添加样式

---
+++ mt-app/components/public/header/geo.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="m-geo">
+    <i class="el-icon-location"/>北京市
+    <nuxt-link></nuxt-link>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- mt-app/layouts/default.vue
+++ mt-app/layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
+  <el-container>
+    <el-header>Header</el-header>
     <nuxt />
-  </div>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
